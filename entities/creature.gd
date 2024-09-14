5f274f9397fb4893328dcd69f370b7a6184dd08f
Baconinvader <shepherd.sam@live.co.uk>
2024-09-14 07:36:03 +0000

    Add enemies

--- entities/creature.gd
+++ entities/creature.gd
@@ -6,12 +6,14 @@ class_name Creature
 ## How fast the creature moves per second
 @export var move_speed:float = 96.0
 
+
 ## Direction creature is moving in, should be normalized
 var move_dir:Vector2 = Vector2(0,0)
 
 func _physics_process(_delta):
 	velocity = move_dir*move_speed
 	move_and_slide()
+
 	
 ## Override as required, Called when creature dies
 func die():
