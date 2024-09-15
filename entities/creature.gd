3ce5875225788c7a04cdfa79f6ebc271e16678cb
Baconinvader <shepherd.sam@live.co.uk>
2024-09-15 07:29:55 +0000

    Animated sprite implementation

--- entities/creature.gd
+++ entities/creature.gd
@@ -14,6 +14,33 @@ func _physics_process(_delta):
 	if not freed_loop:
 		velocity = move_dir*move_speed
 		move_and_slide()
+		
+	set_sprite()
+		
+## Set the direction sprite
+func set_sprite():
+	if not $sprite.sprite_frames:
+		return
+	
+	var best_dot:float = 0.1
+	var best_dir:String = "default"
+	var dir_names:Array[String] = ["up","down","left","right",
+	"up_left", "up_right", "down_left", "down_right"]
+	var dirs:Array[Vector2] = [Vector2(0,-1), Vector2(0,1), Vector2(-1,0), Vector2(1,0),
+	Vector2(-1,-1), Vector2(1,-1), Vector2(-1,1), Vector2(1,1)]
+	
+	var i:int = 0
+	for dir:Vector2 in dirs:
+		if move_dir.dot(dir) > best_dot:
+			best_dot = move_dir.dot(dir)
+			best_dir = dir_names[i]
+		i += 1
+		
+	if best_dir != "default":
+		$sprite.animation = best_dir
+		$sprite.play()
+	else:
+		$sprite.stop()
 
 	
 ## Override as required, Called when creature dies
